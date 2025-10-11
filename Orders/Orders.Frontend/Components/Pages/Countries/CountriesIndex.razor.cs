c3ec566b4761c27ee1a0bc23311d88fc9dab9d2b
SantiagoL-A <santylopez2001@gmail.com>
2025-10-11 18:12:55 -0500

    basic Countries index

---
+++ Orders/Orders.Frontend/Components/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.shared.Entities;
+
+namespace Orders.Frontend.Components.Pages.Countries;
+
+public partial class CountriesIndex
+{
+    private List<Country>? countries;
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var httpResult = await Repository.GetAsync<List<Country>>("/api/countries");
+        countries = httpResult.Response;
+    }
+}
---
+++ Orders/Orders.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Orders.Frontend.Repositories;
+
+public interface IRepository
+{
+    Task<HttpResponseWrapper<T>> GetAsync<T>(string Url);
+
+    Task<HttpResponseWrapper<object>> PostAsync<T>(string Url, T model);
+
+    Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+
+    Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+
+    Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+    Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+}
